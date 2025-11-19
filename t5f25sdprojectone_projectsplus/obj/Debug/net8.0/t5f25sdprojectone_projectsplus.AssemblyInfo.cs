3254a760d3a3c7a7ba4c81504c079a29756e9815
cbugarin <cbugarin@my.centennialcollge.ca>
2025-11-19 13:06:48 -0500

    Update icons and UI changes

--- t5f25sdprojectone_projectsplus/obj/Debug/net8.0/t5f25sdprojectone_projectsplus.AssemblyInfo.cs
+++ t5f25sdprojectone_projectsplus/obj/Debug/net8.0/t5f25sdprojectone_projectsplus.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("t5f25sdprojectone_projectsplus")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8538ec07c331b4b9463318482fee251941264dc1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5937b15e2c70922bef340ba852b051c0386ce185")]
 [assembly: System.Reflection.AssemblyProductAttribute("t5f25sdprojectone_projectsplus")]
 [assembly: System.Reflection.AssemblyTitleAttribute("t5f25sdprojectone_projectsplus")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
