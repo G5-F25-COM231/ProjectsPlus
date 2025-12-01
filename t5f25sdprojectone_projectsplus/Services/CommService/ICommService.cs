1062b1e6aed7cf1e996d933b8bc44fefc5801207
wnforne@gmail.com <mnforne@gmail.com>
2025-11-30 19:56:58 -0500

    BE : add contracts and DTOs for the github interface

---
+++ t5f25sdprojectone_projectsplus/Services/CommService/ICommService.cs
@@ -0,0 +1,6 @@
+﻿namespace t5f25sdprojectone_projectsplus.Services.CommService
+{
+    public class ICommService
+    {
+    }
+}
--- t5f25sdprojectone_projectsplus/obj/Debug/net8.0/t5f25sdprojectone_projectsplus.AssemblyInfo.cs
+++ t5f25sdprojectone_projectsplus/obj/Debug/net8.0/t5f25sdprojectone_projectsplus.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("t5f25sdprojectone_projectsplus")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37aee8a068ad91ce7b5b6e189b4646807f7c1a8a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97aa39d5d34bc1945803da7d71de949f993362c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("t5f25sdprojectone_projectsplus")]
 [assembly: System.Reflection.AssemblyTitleAttribute("t5f25sdprojectone_projectsplus")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
