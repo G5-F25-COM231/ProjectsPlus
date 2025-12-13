dc12f985232d5a09e76d7f9fa3c76d6ef82a483f
wnforne@gmail.com <mnforne@gmail.com>
2025-12-13 06:24:14 -0500

    remove duplicate .gitignore file

--- t5f25sdprojectone_projectsplus/obj/Debug/net8.0/t5f25sdprojectone_projectsplus.AssemblyInfo.cs
+++ t5f25sdprojectone_projectsplus/obj/Debug/net8.0/t5f25sdprojectone_projectsplus.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("t5f25sdprojectone_projectsplus")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+377028d64fb16593212e12e7a4b7e90c57952a47")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ce47c6530ff944e1bfe1f06be022a9b6a6e680c")]
 [assembly: System.Reflection.AssemblyProductAttribute("t5f25sdprojectone_projectsplus")]
 [assembly: System.Reflection.AssemblyTitleAttribute("t5f25sdprojectone_projectsplus")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
